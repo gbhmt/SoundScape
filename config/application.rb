b49eca5e79476b2cc2975fa8eb4c3c9ef95e61a1
Taylor Herron <taylorbherron@gmail.com>
2016-10-06 01:57:46 -0400

    Trying to fix missing region error

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module SoundScape
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => ENV["AWS_REGION"]
+        :s3_region => ENV["s3_region"]
       }
     }
 
